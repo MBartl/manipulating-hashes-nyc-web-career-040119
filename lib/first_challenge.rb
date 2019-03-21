b772f63b644b089db32b35ce00b321a8c330de84
Matthew Bartl <mattbartl@gmail.com>
2019-03-21 00:54:51 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
   contacts.each do |key, value|
-    if key == :favorite_icecream_flavors
+    if key == "favorite_icecream_flavors"
       value.delete_if { |x| x == strawberry }
     end
   end
